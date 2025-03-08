2846a6734adc15f5bc88bc1404e4683b0ad2adca
Ahamed Maahin Siddiq Rahuman <ahamaasidrah@gmail.com>
2025-03-08 13:21:17 -0700

    Added a Character Controller:
    Added a Character Controller through the script for the player and as of now assigned Horizontal movements to the player.

--- Assets/Scripts/CharcaterController.cs
+++ Assets/Scripts/CharcaterController.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class CharcaterController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private CharacterController playerController;
+    private Vector3 playerVelocity;
+    private bool playerGrounded;
+    private float playerSpeed = 7f;
 
-    // Update is called once per frame
+    private float playerJump = 2f;
+    private float playerGravity = -4f;
+
+    private void Start()
+    {
+        playerController = gameObject.AddComponent<CharacterController>();
+    }
+    
     void Update()
     {
+        playerGrounded = playerController.isGrounded;
+        if (playerGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
         
+        //For Moving Left and Right deirections:
+        Vector3 movement = new Vector3(Input.GetAxis ("Horizontal"), 0);
+        playerController.Move (movement * playerSpeed * Time.deltaTime);
     }
 }
