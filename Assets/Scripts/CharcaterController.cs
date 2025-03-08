7a95745a4d853ac5a0b58aeea48156c15537497e
Ahamed Maahin Siddiq Rahuman <ahamaasidrah@gmail.com>
2025-03-08 15:00:35 -0700

    Vertical Movement and Camera:
    Added vertical movements to the player and made the camera to follow along with the player as it moves.

--- Assets/Scripts/CharcaterController.cs
+++ Assets/Scripts/CharcaterController.cs
@@ -7,15 +7,17 @@ public class CharcaterController : MonoBehaviour
     private bool playerGrounded;
     private float playerSpeed = 7f;
 
-    private float playerJump = 2f;
-    private float playerGravity = -4f;
+    //private float playerJump = 2f;
+   
 
     private void Start()
     {
+        //To assign the playerController as the CharcaterController:
         playerController = gameObject.AddComponent<CharacterController>();
     }
     
     void Update()
+    //To check whether the player is grounded?
     {
         playerGrounded = playerController.isGrounded;
         if (playerGrounded && playerVelocity.y < 0)
@@ -24,7 +26,14 @@ public class CharcaterController : MonoBehaviour
         }
         
         //For Moving Left and Right deirections:
-        Vector3 movement = new Vector3(Input.GetAxis ("Horizontal"), 0);
-        playerController.Move (movement * playerSpeed * Time.deltaTime);
+        Vector3 movementSides = new Vector3(Input.GetAxis ("Horizontal"), 0, Input.GetAxis("Vertical"));
+        playerController.Move (movementSides * playerSpeed * Time.deltaTime);
+        
+        /* This is one of the method I tried out to give vertcial movements seperately but couldnt able to figure it out.
+         It would be helpful if I get some ideologies on executing this as I wished.*/
+        //For Moving Straight and Back:
+        // Vector3 moveStraightBack = new Vector3(Input.GetAxis("Vertical"), 0);
+        // playerController.Move(moveStraightBack * playerSpeed * Time.deltaTime);
+        
     }
 }
